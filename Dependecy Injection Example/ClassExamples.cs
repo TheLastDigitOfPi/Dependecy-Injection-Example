fd84b875ae699fbf8c80c8629f80950ef5acb4fc
Daniel Davenport <ddavenportwork10@gmail.com>
2022-07-09 17:15:22 -0700

    Initial commit

---
+++ Dependecy Injection Example/ClassExamples.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependecy_Injection_Example
+{
+    //Classes would normally be seperated into respective files, but this is all for example
+    interface IWeapon
+    {
+        void AttackWithMe();
+    }
+
+    interface IHero
+    {
+        void Attack();
+    }
+
+    internal class Sword : IWeapon
+    {
+        public string SwordName { get; set; }
+        public Sword(string swordName)
+        {
+            SwordName = swordName;
+        }
+        public Sword()
+        {
+            SwordName = "Sad sword";
+        }
+
+        public void AttackWithMe()
+        {
+            Console.WriteLine($"{SwordName} slices through the air, devatsting all enemies");
+        }
+    }
+
+    public class HeroThatOnlyUsesSwords : IHero
+    {
+        public string Name { get; set; }
+        public HeroThatOnlyUsesSwords(string name)
+        {
+            Name = name;
+        }
+        public HeroThatOnlyUsesSwords() { Name = "Generic Hero. No name given"; }
+
+        public void Attack()
+        {
+            Sword sword = new Sword("Excalibur");
+            Console.WriteLine($"{Name} prepares themselves for the battle");
+            sword.AttackWithMe();
+        }
+    }
+
+    internal class HeroThatCanUseAnyWeapon : IHero
+    {
+        public string Name { get; set; }
+        public IWeapon MyWeapon { get; set; }
+
+        public HeroThatCanUseAnyWeapon(string name, IWeapon myWeapon)
+        {
+            Name = name;
+            MyWeapon = myWeapon;
+        }
+        public HeroThatCanUseAnyWeapon()
+        {
+            Name = "Mr. Nobody. No name provided";
+            MyWeapon = null;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{Name} prepares to attack");
+            MyWeapon.AttackWithMe();
+        }
+    }
+    internal class Grenade : IWeapon
+    {
+        public string WeaponName { get; set; }
+        public Grenade(string weaponName)
+        {
+            WeaponName = weaponName;
+        }
+        public Grenade()
+        {
+            WeaponName = "Pathetic grenade. No name provided";
+        }
+
+        public void AttackWithMe()
+        {
+            Console.WriteLine($"{WeaponName} sizzles for a moment and then explodes into a shower of deadly metal fragments");
+        }
+    }
+
+    internal class Bullet
+    {
+        public string Name { get; set; }
+        public int GramsOfPoweder { get; set; }
+
+        public Bullet(string name, int gramsOfPoweder)
+        {
+            Name = name;
+            GramsOfPoweder = gramsOfPoweder;
+        }
+    }
+
+    internal class Gun : IWeapon
+    {
+        public string Name { get; set; }
+        public List<Bullet> Bullets { get; set; }
+
+        public Gun(string name, List<Bullet> bullets)
+        {
+            Name = name;
+            Bullets = bullets;
+        }
+
+        public void AttackWithMe()
+        {
+            if(Bullets.Count > 0)
+            {
+                Console.WriteLine($"{Name} fires the round called {Bullets[0].Name}. The victim now has a dealy hold in them");
+                Bullets.RemoveAt(0);
+                return;
+            }
+            Console.WriteLine($"{Name} has no bullets. Nothing happens");
+        }
+    }
+}
---
+++ Dependecy Injection Example/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Dependecy_Injection_Example;
+using Microsoft.Extensions.DependencyInjection;
+
+
+#region Previous Implementation
+
+/*
+HeroThatOnlyUsesSwords hero1 = new("Ultralord");
+hero1.Attack();
+Console.WriteLine();
+
+HeroThatCanUseAnyWeapon hero2 = new("Eregon", new Sword("Brisinger"));
+hero2.Attack();
+Console.WriteLine();
+
+HeroThatCanUseAnyWeapon hero3 = new("The joker", new Grenade("The Pineapple"));
+hero3.Attack();
+Console.WriteLine();
+
+HeroThatCanUseAnyWeapon hero4 =
+   new("GI Joe", new Gun("Six Shooter",
+       new List<Bullet>
+       {
+           new Bullet("Silver Slug", 10),
+           new Bullet("Lead Ball", 20),
+           new Bullet("Rusty Nail", 3),
+           new Bullet("Hollow Point", 5)
+       }
+   ));
+
+for (int i = 0; i < 5; i++)
+{
+   hero4.Attack();
+}
+*/
+#endregion
+
+
+ServiceCollection services = new ServiceCollection();
+
+//services.AddTransient<IWeapon, Grenade>(grenade => new Grenade("Exploding Pen"));
+//services.AddTransient<IWeapon, Sword>(s => new Sword("The Sword of Gryffindor"));
+services.AddTransient<IWeapon, Gun>(g => new Gun("Six Shooter",
+       new List<Bullet>
+       {
+           new Bullet("Silver Slug", 10),
+           new Bullet("Lead Ball", 20),
+           new Bullet("Rusty Nail", 3),
+           new Bullet("Hollow Point", 5)
+       }));
+
+services.AddTransient<IHero, HeroThatCanUseAnyWeapon>(
+    hero => new HeroThatCanUseAnyWeapon("Jonny Englinsh", hero.GetService<IWeapon>())
+    );
+
+ServiceProvider provider = services.BuildServiceProvider();
+
+var hero5 = provider.GetService<IHero>();
+
+hero5.Attack();
+Console.WriteLine();
+Console.ReadLine();
+
